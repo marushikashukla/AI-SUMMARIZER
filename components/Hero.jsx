d9301b3015b12e97dd91039776e019760f0e44ea
Marushika Shukla <86406035+marushikashukla@users.noreply.github.com>
2023-06-02 18:02:44 +0530

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -11,7 +11,7 @@ export const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/marushikashukla", "_blank")
           }
           className='black_btn'
         >
